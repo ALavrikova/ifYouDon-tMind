d4ec857d0822607f29b5af06f139c80f63f08032
Anastasia Lavrikova <anastasialavrikova@Anastasias-MacBook-Air.local>
2017-08-22 16:31:12 -0700

    Updated with Arsia about edge cases - second commit

---
+++ IfYouDontMind-Ana-Arsia.js
@@ -0,0 +1,42 @@
+function ifYouDontMind(HOUR,MINUTE,PERIOD) {
+    var str = "It's ";
+    if (MINUTE <= 30) {
+        str += "just after " + HOUR;
+    }
+    else {
+        if (HOUR === 12){
+            str += "almost " + 1;
+        }
+        else{
+            str += "almost " + (HOUR + 1);
+        }
+    }
+    if (PERIOD === "AM") {
+        if (HOUR === 11) {
+            if(MINUTE > 30) {
+                str += " in the evening.";
+            } 
+            else{
+                str += " in the morning.";
+            }
+        }
+        else {
+            str += " in the morning.";
+        }
+    }
+    else {
+        if (HOUR === 11) {
+            if(MINUTE > 30) {
+                str += " in the morning.";
+            }
+            else{
+                str += " in the evening.";
+            }
+        }
+        else {
+            str += " in the evening.";
+        }
+    }
+    console.log(str);
+    
+}
